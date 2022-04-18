7e42042ea5d2e65f656b204d4e7282309281af02
Cole Langner <clangner@iastate.edu>
2022-04-17 22:33:51 -0500

    skeleton rest

---
+++ src/main/java/com/example/Movie/RestAPI/EndPoints.java
@@ -0,0 +1,27 @@
+package com.example.Movie.RestAPI;
+import java.util.Optional;
+import java.util.Set;
+import java.util.ArrayList;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+@RestController
+public class EndPoints {
+
+    
+@RequestMapping("/")
+public @ResponseBody String test(){
+    return "testing";
+}
+
+
+
+}
